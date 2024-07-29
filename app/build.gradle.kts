70324072fc2bb89ad351894e2a1a4733ff6b79df
mxyffff <rua0254@naver.com>
2024-07-30 01:09:23 +0900

    build.gradle과 settgins.gradle 파일 수정안

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,18 +35,31 @@ android {
     }
     buildFeatures { // 뷰 바인딩 환경 세팅
         viewBinding = true
+        dataBinding = true
     }
 }
 
 dependencies {
 
+    implementation ("com.google.android.material:material:1.4.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // 네이버 지도 SDK
+    implementation("com.naver.maps:map-sdk:3.18.0")
+
+    // 네이버지도 위치 추적
+    implementation("com.google.android.gms:play-services-location:21.1.0")
 }
